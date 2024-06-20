f4240388223ef2ca026e409de999bf8c04c94451
parkrocket <shinbyro@kakao.com>
2024-06-20 18:39:47 +0900

    xxx

--- nodejs-server/src/index.js
+++ nodejs-server/src/index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 5000; // 사용할 포트 번호
 
 app.get('/api/hello', (req, res) => {
-    res.send({ message: 'Hello from Node.js!!' });
+    res.send({ message: 'Hello from Node.js!!!' });
 });
 
 app.listen(port, () => {
