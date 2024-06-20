80622abb35b2061f7a50edcd163250f165c36664
parkrocket <shinbyro@kakao.com>
2024-06-20 17:48:49 +0900

    배포

---
+++ nodejs-server/src/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = 5000; // 사용할 포트 번호
+
+app.get('/api/hello', (req, res) => {
+    res.send({ message: 'Hello from Node.js!' });
+});
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
