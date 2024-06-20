dc7e06ccf9c186ba48c1c8286d86a4dca3e30896
parkrocket <shinbyro@kakao.com>
2024-06-20 17:08:54 +0900

    build test

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -6,7 +6,7 @@ function App() {
         <div className="App">
             <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo" />
-                <p>리액트 배포 테스트입니다.</p>
+                <p>리액트 배포 테스트입니다!!!</p>
                 <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
                     Learn React
                 </a>
