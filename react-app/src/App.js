ee01f8292e6f21ed0eb2733209c62d0fa10f3b81
parkrocket <shinbyro@kakao.com>
2024-06-21 16:02:28 +0900

    통신

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -3,9 +3,11 @@ import React, { useEffect, useState } from 'react';
 function App() {
     const [message, setMessage] = useState('');
 
+    const apiUrl = process.env.REACT_APP_API_URL;
+
     useEffect(() => {
         // 로컬 Node.js 서버의 API 엔드포인트 호출
-        fetch('http://localhost:5000/api/hello')
+        fetch(apiUrl)
             .then((response) => response.json())
             .then((data) => setMessage(data.message))
             .catch((error) => console.error('Error fetching data:', error));
