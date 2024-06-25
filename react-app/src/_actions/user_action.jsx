3c81d970659b608684977ea2329fa10c9be1690e
parkrocket <shinbyro@kakao.com>
2024-06-25 18:39:54 +0900

    20240625박성현

--- react-app/src/_actions/user_action.jsx
+++ react-app/src/_actions/user_action.jsx
@@ -1,14 +1,25 @@
 import axios from 'axios';
-import { SERVER_URL } from './Config';
+import { SERVER_URL } from '../Config';
 
 import { LOGIN_USER } from './types';
 
 export function loginUser(dataTosubmit) {
-    //const request = axios.post(`${SERVER_URL}/api/users/login`, dataTosubmit).then((response) => response.data);
+    return (dispatch) => {
+        return new Promise((resolve) => {
+            const request = { success: true, mb_id: 'admin' }; // 임시로 비동기 응답을 모방
+            dispatch({
+                type: LOGIN_USER,
+                payload: request,
+            });
+            resolve(request);
+        });
+    };
 
-    const request = { success: true, mb_id: 'admin' };
+    //const request = axios.post(`${SERVER_URL}/api/users/login`, dataTosubmit).then((response) => response.data);
+    /*
     return {
         type: LOGIN_USER,
         payload: request,
     };
+    */
 }
--- react-app/src/pages/Login.js
+++ react-app/src/pages/Login.js
@@ -1,7 +1,38 @@
 import React from 'react';
+import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { loginUser } from '../_actions/user_action';
 
 function Login() {
-    return <div>Login</div>;
+    const [email, setEamil] = useState('');
+    const [password, setPassword] = useState('');
+
+    const dispatch = useDispatch();
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+
+        const loginData = {
+            email,
+            password,
+        };
+
+        console.log(email, password);
+
+        dispatch(loginUser(loginData)).then((response) => {
+            console.log(response);
+        });
+    };
+
+    return (
+        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', width: '100%', height: '100vh' }}>
+            <form style={{ display: 'flex', flexDirection: 'column' }} onSubmit={onSubmit}>
+                <input type="text" placeholder="ID" value={email} onChange={(e) => setEamil(e.target.value)} />
+                <input type="password" placeholder="PW" value={password} onChange={(e) => setPassword(e.target.value)} />
+                <button type="submit">Login</button>
+            </form>
+        </div>
+    );
 }
 
 export default Login;
