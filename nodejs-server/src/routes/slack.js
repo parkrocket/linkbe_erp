af69b95af1bec7a03b8e1b5144d62fdc6b4c201c
parkrocket <shinbyro@kakao.kr>
2024-07-10 11:24:08 +0900

    slack home

--- nodejs-server/src/routes/slack.js
+++ nodejs-server/src/routes/slack.js
@@ -1,16 +1,14 @@
 const express = require('express');
 const router = express.Router();
-const slackApp = require('../utils/slack');
 const { WebClient } = require('@slack/web-api');
+const dotenv = require('dotenv');
+
+dotenv.config();
 
 const token = process.env.SLACK_BOT_TOKEN;
 const client = new WebClient(token);
 
 router.post('/events', async (req, res) => {
-    await slackApp.requestListener()(req, res);
-});
-
-router.post('/home', async (req, res) => {
     const { type, challenge, event } = req.body;
 
     if (type === 'url_verification') {
@@ -19,40 +17,49 @@ router.post('/home', async (req, res) => {
     } else if (type === 'event_callback' && event.type === 'app_home_opened') {
         const userId = event.user;
 
-        console.log(event);
-
-        // Block Kit structure for home tab
-        const view = {
-            type: 'home',
-            callback_id: 'home_view',
-            blocks: [
-                {
-                    type: 'section',
-                    text: {
-                        type: 'mrkdwn',
-                        text: "Welcome to your _App's Home_!",
-                    },
-                },
-                {
-                    type: 'divider',
-                },
-                {
-                    type: 'section',
-                    text: {
-                        type: 'mrkdwn',
-                        text: "*Here's a cool image:*",
-                    },
-                },
-            ],
-        };
-
-        // Publish the view
         try {
-            await client.views.publish({
-                user_id: userId,
-                view: view,
-            });
-            res.status(200).send();
+            // 사용자 정보 가져오기
+            const userInfo = await client.users.info({ user: userId });
+
+            if (userInfo.ok) {
+                const userEmail = userInfo.user.profile.email;
+
+                // Block Kit structure for home tab
+                const view = {
+                    type: 'home',
+                    callback_id: 'home_view',
+                    blocks: [
+                        {
+                            type: 'section',
+                            text: {
+                                type: 'mrkdwn',
+                                text: `Welcome to your _App's Home_, ${userEmail}!`,
+                            },
+                        },
+                        {
+                            type: 'divider',
+                        },
+                        {
+                            type: 'section',
+                            text: {
+                                type: 'mrkdwn',
+                                text: "*Here's a cool image:*",
+                            },
+                        },
+                    ],
+                };
+
+                // Publish the view
+                await client.views.publish({
+                    user_id: userId,
+                    view: view,
+                });
+
+                res.status(200).send();
+            } else {
+                console.error('Error fetching user info:', userInfo.error);
+                res.status(500).send();
+            }
         } catch (error) {
             console.error('Error publishing view:', error);
             res.status(500).send();
