00d36c33c74ff39f7f1b713fed8a540917e5a0cd
parkrocket <shinbyro@kakao.kr>
2024-07-10 15:40:03 +0900

    slack home

--- nodejs-server/src/routes/slack.js
+++ nodejs-server/src/routes/slack.js
@@ -8,24 +8,25 @@ const requestIp = require('request-ip');
 const crypto = require('crypto');
 
 const algorithm = 'aes-256-cbc';
-const secretKey = 'linkbeflatformlinkbeflatformlink';
-const iv = crypto.randomBytes(16);
+const secretKey = 'linkbeflatformlinkbeflatformlink'; // 32바이트 키
+const ivLength = 16; // AES 블록 크기
 
 const encrypt = (text) => {
-    const cipher = crypto.createCipheriv(algorithm, Buffer.from(secretKey, 'hex'), iv);
-    let encrypted = cipher.update(text);
-    encrypted = Buffer.concat([encrypted, cipher.final()]);
-    return iv.toString('hex') + ':' + encrypted.toString('hex');
+    const iv = crypto.randomBytes(ivLength); // 암호화마다 다른 IV 사용
+    const cipher = crypto.createCipheriv(algorithm, Buffer.from(secretKey), iv);
+    let encrypted = cipher.update(text, 'utf8', 'hex');
+    encrypted += cipher.final('hex');
+    return iv.toString('hex') + ':' + encrypted;
 };
 
 const decrypt = (text) => {
     const textParts = text.split(':');
     const iv = Buffer.from(textParts.shift(), 'hex');
     const encryptedText = Buffer.from(textParts.join(':'), 'hex');
-    const decipher = crypto.createDecipheriv(algorithm, Buffer.from(secretKey, 'hex'), iv);
-    let decrypted = decipher.update(encryptedText);
-    decrypted = Buffer.concat([decrypted, decipher.final()]);
-    return decrypted.toString();
+    const decipher = crypto.createDecipheriv(algorithm, Buffer.from(secretKey), iv);
+    let decrypted = decipher.update(encryptedText, 'hex', 'utf8');
+    decrypted += decipher.final('utf8');
+    return decrypted;
 };
 
 const token = process.env.SLACK_BOT_TOKEN;
