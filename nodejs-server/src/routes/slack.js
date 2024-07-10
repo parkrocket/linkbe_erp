c5a28d2161ed2bd3a7df0c54a6ee962926045c7e
parkrocket <shinbyro@kakao.kr>
2024-07-10 13:24:35 +0900

    slack home

--- nodejs-server/src/routes/slack.js
+++ nodejs-server/src/routes/slack.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const User = require('../models/userModel');
+const moment = require('moment');
 const router = express.Router();
 const slackApp = require('../utils/slack');
 const { WebClient } = require('@slack/web-api');
@@ -20,48 +22,58 @@ router.post('/home', async (req, res) => {
         const userId = event.user;
 
         const userInfo = await client.users.info({ user: userId });
-
-        console.log(userInfo.user.profile.email);
+        const date = moment().format('YYYY-MM-DD');
 
         // Block Kit structure for home tab
         if (userInfo.ok) {
             const userEmail = userInfo.user.profile.email;
 
-            const view = {
-                type: 'home',
-                callback_id: 'home_view',
-                blocks: [
-                    {
-                        type: 'section',
-                        text: {
-                            type: 'mrkdwn',
-                            text: `Welcome to your _App's Home_, ${userEmail}!`,
-                        },
-                    },
-                    {
-                        type: 'divider',
-                    },
-                    {
-                        type: 'section',
-                        text: {
-                            type: 'mrkdwn',
-                            text: "*Here's a cool image:*",
-                        },
-                    },
-                ],
-            };
+            User.findByEmail(userEmail, async (err, user) => {
+                //return res.status(500).json({ message: 'Login successful', user: user });
 
-            // Publish the view
-            try {
-                await client.views.publish({
-                    user_id: userId,
-                    view: view,
-                });
-                res.status(200).send();
-            } catch (error) {
-                console.error('Error publishing view:', error);
-                res.status(500).send();
-            }
+                if (err) {
+                    return res.status(200).json({ refreshSuccess: false, error: 'Database query error' });
+                }
+                if (!user) {
+                    return res.status(200).json({ refreshSuccess: false, error: 'User not found' });
+                }
+
+                const view = {
+                    type: 'home',
+                    callback_id: 'home_view',
+                    blocks: [
+                        {
+                            type: 'section',
+                            text: {
+                                type: 'mrkdwn',
+                                text: `${date}일 입니다. ${user.user_name}님!`,
+                            },
+                        },
+                        {
+                            type: 'divider',
+                        },
+                        {
+                            type: 'section',
+                            text: {
+                                type: 'mrkdwn',
+                                text: "*Here's a cool image:*",
+                            },
+                        },
+                    ],
+                };
+
+                // Publish the view
+                try {
+                    await client.views.publish({
+                        user_id: userId,
+                        view: view,
+                    });
+                    res.status(200).send();
+                } catch (error) {
+                    console.error('Error publishing view:', error);
+                    res.status(500).send();
+                }
+            });
         } else {
             console.log('유저정보 에러');
         }
