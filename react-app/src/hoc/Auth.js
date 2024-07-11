cb6c1bc6f6adbc10e21e91af108139b430a899e7
parkrocket <shinbyro@kakao.kr>
2024-07-11 10:29:27 +0900

    slack home

--- react-app/src/hoc/Auth.js
+++ react-app/src/hoc/Auth.js
@@ -1,5 +1,6 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { useCookies } from 'react-cookie';
 import { auth } from '../_actions/user_action';
@@ -10,33 +11,31 @@ function Auth(ChildrenComponent, option, adminRoute = false, menu = 0, subMenu =
     function AuthenticationCheck() {
         const navigate = useNavigate();
         const dispatch = useDispatch();
-        const [cookies, setCookie, removeCookie] = useCookies(['x_auth']); // setCookie, removeCookie 추가
+        const [cookies] = useCookies(['x_auth']);
+
         const [view, setView] = useState(false);
+        console.log(cookies);
 
         useEffect(() => {
-            // 쿠키 값 확인
-            console.log('Initial cookies: ', cookies);
-
-            if (!cookies.x_auth) {
-                alert('로그인이 필요합니다!');
-                navigate('/login');
-                return;
-            }
-
             dispatch(auth(cookies)).then((response) => {
-                console.log('Auth response cookies: ', cookies);
-
+                console.log(cookies);
                 if (response.payload.isAuth) {
+                    //console.log(response.payload.isAdmin);
+                    //로그인 했음
                     if (adminRoute && !response.payload.isAdmin) {
+                        //관리자 페이지를 관리자 권한이 없는 사람이 들어가려고 할때
                         alert('관리자가 아닙니다.');
                         navigate('/');
                     } else {
+                        //로그인 안했을때 가능한 페이지를 진입하려고 할때
                         if (option === false) {
                             navigate('/');
                         }
+
                         setView(true);
                     }
                 } else {
+                    //로그인 안했음
                     if (option === true) {
                         alert('로그인이 필요합니다!');
                         navigate('/login');
@@ -47,20 +46,24 @@ function Auth(ChildrenComponent, option, adminRoute = false, menu = 0, subMenu =
 
             if (adminRoute === true) {
                 const data = { menu, subMenu };
+
                 //dispatch(adminMenu(data));
             }
-        }, [cookies, dispatch, navigate]);
+        }, [cookies, dispatch]);
 
+        // null -> 모두다 가능
+        // true -> 로그인 한 인원만
+        // false -> 로그인 안한 인원만
         if (view === false) {
             return (
                 <div>
-                    <Empty />
+                    <Empty></Empty>
                 </div>
             );
         } else {
             return (
                 <div>
-                    <ChildrenComponent />
+                    <ChildrenComponent></ChildrenComponent>
                 </div>
             );
         }
@@ -68,7 +71,7 @@ function Auth(ChildrenComponent, option, adminRoute = false, menu = 0, subMenu =
 
     return (
         <div>
-            <AuthenticationCheck />
+            <AuthenticationCheck></AuthenticationCheck>
         </div>
     );
 }
