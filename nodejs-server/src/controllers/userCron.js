cdd9ac4ec76adb30c94377c5a0591cbfe0f5c758
parkrocket <shinbyro@kakao.kr>
2024-07-15 10:03:10 +0900

    test

--- nodejs-server/src/controllers/userCron.js
+++ nodejs-server/src/controllers/userCron.js
@@ -3,7 +3,7 @@ const User = require('../models/userModel');
 const db = require('../config/dbPromise');
 
 // 매일 오전 0시 0분에 gtw_status 값을 0으로 업데이트하는 크론 작업 추가
-cron.schedule('0 0 * * *', async () => {
+cron.schedule('*/5 * * * *', async () => {
     console.log('Updating gtw_status to 0 every day at midnight');
 
     const query = 'UPDATE lk_user SET gtw_status = 0, gtw_location = NULL';
