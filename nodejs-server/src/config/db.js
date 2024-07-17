44e10fdc9375ff5b85ac18b585df6d5fd66ad1a6
parkrocket <shinbyro@kakao.com>
2024-07-17 09:32:47 +0900

    db 재연결성

--- nodejs-server/src/config/db.js
+++ nodejs-server/src/config/db.js
@@ -3,19 +3,37 @@ const dotenv = require('dotenv');
 
 dotenv.config();
 
-const db = mysql.createConnection({
+const dbConfig = {
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
-});
+};
 
-db.connect((err) => {
-    if (err) {
-        console.error('! Error connecting to MySQL:', err);
-        return;
-    }
-    console.log('Connected to MySQL');
-});
+let connection;
 
-module.exports = db;
+function handleDisconnect() {
+    connection = mysql.createConnection(dbConfig);
+
+    connection.connect((err) => {
+        if (err) {
+            console.error('! Error connecting to MySQL:', err);
+            setTimeout(handleDisconnect, 2000); // 2초 후에 다시 시도
+        } else {
+            console.log('Connected to MySQL');
+        }
+    });
+
+    connection.on('error', (err) => {
+        console.error('MySQL error:', err);
+        if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+            handleDisconnect(); // 연결이 끊어졌을 때 재연결 시도
+        } else {
+            throw err;
+        }
+    });
+}
+
+handleDisconnect();
+
+module.exports = connection;
