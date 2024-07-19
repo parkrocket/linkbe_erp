71e86908e9657d654ecb5f51b6b7ea884b8d956d
parkrocket <shinbyro@kakao.kr>
2024-07-19 16:45:08 +0900

    모듈분리

--- nodejs-server/src/routes/slack.js
+++ nodejs-server/src/routes/slack.js
@@ -5,6 +5,6 @@ const router = express.Router();
 router.post('/events', handleSlackEvents);
 router.post('/home', handleHomeView);
 router.get('/gtwCheck', handleGtwCheck);
-router.post('/interactions', handleInteractions);
+router.post('/interactions',express.urlencoded({ extended: true }), handleInteractions);
 
 module.exports = router;
