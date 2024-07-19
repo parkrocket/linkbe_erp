92d954dd4382c096e15de1d015355eff3eb51e90
parkrocket <shinbyro@kakao.kr>
2024-07-19 16:41:04 +0900

    모듈분리

--- nodejs-server/src/utils/slack.js
+++ nodejs-server/src/utils/slack.js
@@ -2,11 +2,15 @@ const { WebClient } = require('@slack/web-api');
 const moment = require('moment');
 
 const token = process.env.SLACK_BOT_TOKEN; // Bot User OAuth Token을 사용
-const web = new WebClient(token);
+const client = new WebClient(token);
+
+
+
+
 
 const sendSlackMessage = async (channel, text) => {
     try {
-        await web.chat.postMessage({
+        await client.chat.postMessage({
             channel: channel,
             text: text,
         });
