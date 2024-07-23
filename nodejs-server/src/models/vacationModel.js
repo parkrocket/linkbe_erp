09156baf202412878ab2ba7dc6fb368bf3c20e9c
parkrocket <shinbyro@kakao.kr>
2024-07-23 09:44:45 +0900

    date format

--- nodejs-server/src/models/vacationModel.js
+++ nodejs-server/src/models/vacationModel.js
@@ -5,14 +5,16 @@ const { sendSlackMessage } = require('../utils/slack');
 
 const Vca = {};
 
-
-const auths = new google.auth.JWT(process.env.GOOGLE_CLIENT_EMAIL, null, process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n'), [
-    'https://www.googleapis.com/auth/calendar',
-]);
-
+const auths = new google.auth.JWT(
+    process.env.GOOGLE_CLIENT_EMAIL,
+    null,
+    process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+    ['https://www.googleapis.com/auth/calendar'],
+);
 
 Vca.create = (userId, type, date, eventId, callback) => {
-    query = 'INSERT INTO lk_vacation (user_id, type ,date, va_datetime, calendar_id) VALUES (?, ?, ?, NOW(), ?)';
+    query =
+        'INSERT INTO lk_vacation (user_id, type ,date, va_datetime, calendar_id) VALUES (?, ?, ?, NOW(), ?)';
     queryParams = [userId, type, date, eventId];
 
     db.query(query, queryParams, (err, results) => {
@@ -26,7 +28,8 @@ Vca.create = (userId, type, date, eventId, callback) => {
 };
 
 Vca.findById = (userId, callback) => {
-    query = 'SELECT * FROM lk_vacation LEFT JOIN lk_user ON lk_vacation.user_id = lk_user.user_id WHERE lk_vacation.user_id =? AND lk_vacation.date >= NOW()';
+    query =
+        'SELECT * FROM lk_vacation LEFT JOIN lk_user ON lk_vacation.user_id = lk_user.user_id WHERE lk_vacation.user_id =? AND lk_vacation.date >= NOW()';
     queryParams = [userId];
 
     db.query(query, queryParams, (err, results) => {
@@ -39,9 +42,9 @@ Vca.findById = (userId, callback) => {
     });
 };
 
-Vca.findByAll = (userId,callback) => {
-    const query = 'SELECT * FROM lk_vacation LEFT JOIN lk_user ON lk_vacation.user_id = lk_user.user_id WHERE lk_vacation.date >= NOW() ORDER BY lk_vacation.date ASC';
-
+Vca.findByAll = (userId, callback) => {
+    const query =
+        'SELECT * FROM lk_vacation LEFT JOIN lk_user ON lk_vacation.user_id = lk_user.user_id WHERE lk_vacation.date >= CURDATE() ORDER BY lk_vacation.date ASC';
 
     db.query(query, (err, results) => {
         if (err) {
@@ -53,19 +56,21 @@ Vca.findByAll = (userId,callback) => {
     });
 };
 
-
 Vca.cancel = (id, stipNumber, vacaNumber, userId, calendar_id, type) => {
     return new Promise((resolve, reject) => {
         const deleteQuery = 'DELETE FROM lk_vacation WHERE id = ?';
-        
+
         db.query(deleteQuery, [id], async (err, results) => {
             if (err) {
                 console.log(err);
                 return reject(err);
             }
 
-            if(calendar_id){
-                const calendar = google.calendar({ version: 'v3', auth: auths });
+            if (calendar_id) {
+                const calendar = google.calendar({
+                    version: 'v3',
+                    auth: auths,
+                });
 
                 try {
                     await calendar.events.delete({
@@ -105,14 +110,13 @@ Vca.cancel = (id, stipNumber, vacaNumber, userId, calendar_id, type) => {
                     return reject(err);
                 }
 
-
                 const vacaType =
-                {
-                    half: '반차',
-                    day: '연차',
-                    home: '재택',
-                    vacation: '휴가',
-                }[type] || '알수없음';
+                    {
+                        half: '반차',
+                        day: '연차',
+                        home: '재택',
+                        vacation: '휴가',
+                    }[type] || '알수없음';
 
                 const message = `${userId}님이 ${vacaType}을(를) 취소하셨습니다.`;
 
@@ -123,8 +127,6 @@ Vca.cancel = (id, stipNumber, vacaNumber, userId, calendar_id, type) => {
         });
     });
 };
-
-
 
 Vca.createAsync = (userId, type, date, eventId) => {
     return new Promise((resolve, reject) => {
@@ -137,7 +139,7 @@ Vca.createAsync = (userId, type, date, eventId) => {
     });
 };
 
-Vca.findByIdAsync = (userId) => {
+Vca.findByIdAsync = userId => {
     return new Promise((resolve, reject) => {
         Vca.findById(userId, (err, results) => {
             if (err) {
@@ -149,8 +151,7 @@ Vca.findByIdAsync = (userId) => {
     });
 };
 
-Vca.findByAllAsync = (userId) => {
-
+Vca.findByAllAsync = userId => {
     return new Promise((resolve, reject) => {
         Vca.findByAll(userId, (err, results) => {
             if (err) {
