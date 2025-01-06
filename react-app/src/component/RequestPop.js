413c12273357e781af82ced5a883aba34d44a3e8
parkrocket <shinbyro@kakao.kr>
2025-01-06 14:40:31 +0900

    test

--- react-app/src/component/RequestPop.js
+++ react-app/src/component/RequestPop.js
@@ -1,17 +1,24 @@
 import React, { useState } from 'react';
+import { useSelector } from 'react-redux';
 
 import RequestPopStyle from '../css/RequestPop.module.scss';
 
 function RequestPop({ onClose }) {
+    const user = useSelector(state => state.user);
+
+    console.log(user.userData.user);
+    const userStip = user.userData.user.user_stip;
+    const userVaca = user.userData.user.user_vaca;
+
     return (
         <div className={`${RequestPopStyle.pop}`}>
             <div className={`${RequestPopStyle.bg}`} onClick={onClose}></div>
             <div className={`${RequestPopStyle.cont}`}>
                 <h3 className={`text-align-c padding-b30`}>근태 신청</h3>
                 <div className={`${RequestPopStyle.summary}  padding-b30`}>
-                    사용가능한 연차 개수 : 10.5 <br />
-                    사용가능한 휴가 개수 : 1 <br />
-                    이번 달 재택 가능 개수 : 2
+                    사용가능한 연차 개수 : {userStip} <br />
+                    사용가능한 휴가 개수 : {userVaca} <br />
+                    {/*이번 달 재택 가능 개수 : 2*/}
                 </div>
                 <form action="">
                     <fieldset>
@@ -40,11 +47,16 @@ function RequestPop({ onClose }) {
                             </label>
                             <select name="" id="type" className="display-b">
                                 <option value="연차">연차</option>
+                                <option value="반차">반차</option>
                                 <option value="휴가">휴가</option>
+                                <option value="재택">재택</option>
+
+                                {/*
                                 <option value="반차(오전)">반차(오전)</option>
                                 <option value="반차(오후)">반차(오후)</option>
-                                <option value="재택">재택</option>
+                                
                                 <option value="월차">월차</option>
+                                */}
                             </select>
                         </div>
                         <div
